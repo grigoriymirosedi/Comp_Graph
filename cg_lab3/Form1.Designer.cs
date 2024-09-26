59b66ea96d6c246c30e82aa2aa6de6dd968102f2
PD_Creator <anuchin@sfedu.ru>
2024-09-26 18:23:02 +0300

    lab3 third task solution added

--- cg_lab3/Form1.Designer.cs
+++ cg_lab3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿using System.Drawing;
 
-namespace cg_lab3
+namespace GradientTriangle
 {
     partial class Form1
     {
@@ -30,42 +30,21 @@ namespace cg_lab3
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(800, 450);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDoubleClick);
-            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
-            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
-            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox2);
+            this.ClientSize = new System.Drawing.Size(434, 311);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- cg_lab3/Form1.cs
+++ cg_lab3/Form1.cs
@@ -1,120 +1,150 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace cg_lab3
+namespace GradientTriangle
 {
     public partial class Form1 : Form
     {
-        private Bitmap bitmap;
-        private bool isDrawing = false;
-        private Point previousPoint;
-
+        // Координаты вершин треугольника
+        private PointF[] trianglePoints = new PointF[3];
+        // Цвета для каждой вершины
+        private Color[] triangleColors = new Color[3];
+        // Переменные для перетаскивания вершин
+        private bool isDragging = false;
+        private int draggedPointIndex = -1;
 
         public Form1()
         {
             InitializeComponent();
-            bitmap = new Bitmap(this.Width, this.Height);
-            pictureBox2.Image = bitmap;
+            this.DoubleBuffered = true;  // Для избежания мерцания при перерисовке
+
+            // Инициализация вершин треугольника
+            trianglePoints[0] = new PointF(100, 100);
+            trianglePoints[1] = new PointF(300, 100);
+            trianglePoints[2] = new PointF(200, 300);
+
+            // Инициализация цветов вершин
+            triangleColors[0] = Color.Red;
+            triangleColors[1] = Color.Green;
+            triangleColors[2] = Color.Blue;
+
+            // Подписываемся на события мыши
+            this.MouseDown += Form1_MouseDown;
+            this.MouseMove += Form1_MouseMove;
+            this.MouseUp += Form1_MouseUp;
+            this.Paint += Form1_Paint;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        // Проверка на попадание в вершину треугольника
+        private bool IsPointInSquare(PointF point, PointF vertex, float size = 10)
         {
+            return Math.Abs(point.X - vertex.X) < size && Math.Abs(point.Y - vertex.Y) < size;
         }
 
-       
-
-        private void FillLines(int x, int y, Color targetColor, Color fillColor)
+        // Обработчик нажатия на мышь
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (x < 0 || x >= bitmap.Width || y < 0 || y >= bitmap.Height)
-                return;
-
-            if (bitmap.GetPixel(x, y) != targetColor)
-                return;
-
-            int left = x;
-            int right = x;
-
-            // Находим левую границу строки для заливки
-            while (left > 0 && bitmap.GetPixel(left - 1, y) == targetColor)
+            for (int i = 0; i < trianglePoints.Length; i++)
             {
-                left--;
-            }
-
-            // Находим правую границу строки для заливки
-            while (right < bitmap.Width - 1 && bitmap.GetPixel(right + 1, y) == targetColor)
-            {
-                right++;
-            }
-
-            // Заполняем строку от левой до правой границы
-            for (int i = left; i <= right; i++)
-            {
-                bitmap.SetPixel(i, y, fillColor);
-            }
-
-            // Рекурсивно вызываем для строки выше и строки ниже
-            for (int i = left; i <= right; i++)
-            {
-                if (y > 0 && bitmap.GetPixel(i, y - 1) == targetColor)
+                if (IsPointInSquare(e.Location, trianglePoints[i]))
                 {
-                    FillLines(i, y - 1, targetColor, fillColor);
-                }
-
-                if (y < bitmap.Height - 1 && bitmap.GetPixel(i, y + 1) == targetColor)
-                {
-                    FillLines(i, y + 1, targetColor, fillColor);
+                    isDragging = true;
+                    draggedPointIndex = i;
+                    break;
                 }
             }
         }
 
-        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        // Обработчик перемещения мыши
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            if (isDragging && draggedPointIndex != -1)
             {
-                isDrawing = false;
+                // Перемещаем выбранную вершину
+                trianglePoints[draggedPointIndex] = e.Location;
+                this.Invalidate();  // Перерисовка формы
             }
         }
 
-        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        // Обработчик отпускания кнопки мыши
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            isDragging = false;
+            draggedPointIndex = -1;
+            this.Invalidate();  // Перерисовка формы с новым положением
+        }
+
+        // Обработчик перерисовки
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Отрисовка градиента внутри треугольника
+            FillGradientTriangle(g, trianglePoints, triangleColors);
+
+            // Отрисовка вершин треугольника в виде квадратов для перемещения
+            foreach (PointF point in trianglePoints)
             {
-                isDrawing = true;
-                previousPoint = e.Location; // Запоминаем начальную точку
+                g.FillRectangle(Brushes.Black, point.X - 5, point.Y - 5, 10, 10);
             }
         }
 
-        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        // Функция для заливки треугольника с градиентом
+        private void FillGradientTriangle(Graphics g, PointF[] points, Color[] colors)
         {
-            if (isDrawing)
+            Bitmap bitmap = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            using (Graphics bmpGraphics = Graphics.FromImage(bitmap))
             {
-                using (Graphics g = Graphics.FromImage(bitmap))
+                // Используем интерполяцию для градиента
+                for (int y = 0; y < bitmap.Height; y++)
                 {
-                    Pen pen = new Pen(Color.Black, 4);  
-                    g.DrawLine(pen, previousPoint, e.Location);  
-                    previousPoint = e.Location;  
+                    for (int x = 0; x < bitmap.Width; x++)
+                    {
+                        PointF p = new PointF(x, y);
+                        if (IsPointInTriangle(p, points))
+                        {
+                            Color interpolatedColor = InterpolateColor(p, points, colors);
+                            bitmap.SetPixel(x, y, interpolatedColor);
+                        }
+                    }
                 }
-                pictureBox2.Invalidate(); 
             }
+
+            g.DrawImage(bitmap, 0, 0);
         }
 
-        private void pictureBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        // Функция проверки, находится ли точка внутри треугольника
+        private bool IsPointInTriangle(PointF p, PointF[] triangle)
         {
-            Color targetColor = bitmap.GetPixel(e.X, e.Y);
-            Color fillColor = Color.DarkGreen;
+            float area = TriangleArea(triangle[0], triangle[1], triangle[2]);
+            float area1 = TriangleArea(p, triangle[1], triangle[2]);
+            float area2 = TriangleArea(triangle[0], p, triangle[2]);
+            float area3 = TriangleArea(triangle[0], triangle[1], p);
 
-            if (targetColor != fillColor && targetColor != Color.Black)  
-            {
-                FillLines(e.X, e.Y, targetColor, fillColor);
-                pictureBox2.Invalidate();  // Обновляем изображение
-            }
+            return Math.Abs(area - (area1 + area2 + area3)) < 0.01;
+        }
+
+        // Вычисление площади треугольника
+        private float TriangleArea(PointF p1, PointF p2, PointF p3)
+        {
+            return Math.Abs((p1.X * (p2.Y - p3.Y) + p2.X * (p3.Y - p1.Y) + p3.X * (p1.Y - p2.Y)) / 2.0f);
+        }
+
+        // Функция интерполяции цвета в зависимости от положения точки внутри треугольника
+        private Color InterpolateColor(PointF p, PointF[] points, Color[] colors)
+        {
+            float totalArea = TriangleArea(points[0], points[1], points[2]);
+
+            float w1 = TriangleArea(p, points[1], points[2]) / totalArea;
+            float w2 = TriangleArea(p, points[0], points[2]) / totalArea;
+            float w3 = TriangleArea(p, points[0], points[1]) / totalArea;
+
+            int r = (int)(w1 * colors[0].R + w2 * colors[1].R + w3 * colors[2].R);
+            int g = (int)(w1 * colors[0].G + w2 * colors[1].G + w3 * colors[2].G);
+            int b = (int)(w1 * colors[0].B + w2 * colors[1].B + w3 * colors[2].B);
+
+            return Color.FromArgb(r, g, b);
         }
     }
 }
