cde43477a5dec14d8d762098e6d71be8a964c271
PD_Creator <anuchin@sfedu.ru>
2024-10-08 20:49:23 +0300

    implemented basic features

--- lab4/Form1.Designer.cs
+++ lab4/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace lab4
+﻿using System.Drawing;
+namespace lab4
 {
     partial class Form1
     {
@@ -6,6 +7,21 @@
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.Button btnRotate;
+        private System.Windows.Forms.Button btnScale;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtDx;
+        private System.Windows.Forms.TextBox txtDy;
+        private System.Windows.Forms.TextBox txtAngle;
+        private System.Windows.Forms.TextBox txtScaleX;
+        private System.Windows.Forms.TextBox txtScaleY;
+        private System.Windows.Forms.Label lblDx;
+        private System.Windows.Forms.Label lblDy;
+        private System.Windows.Forms.Label lblAngle;
+        private System.Windows.Forms.Label lblScaleX;
+        private System.Windows.Forms.Label lblScaleY;
 
         /// <summary>
         /// Освободить все используемые ресурсы.
@@ -28,13 +44,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.btnRotate = new System.Windows.Forms.Button();
+            this.btnScale = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtDx = new System.Windows.Forms.TextBox();
+            this.txtDy = new System.Windows.Forms.TextBox();
+            this.txtAngle = new System.Windows.Forms.TextBox();
+            this.txtScaleX = new System.Windows.Forms.TextBox();
+            this.txtScaleY = new System.Windows.Forms.TextBox();
+            this.lblDx = new System.Windows.Forms.Label();
+            this.lblDy = new System.Windows.Forms.Label();
+            this.lblAngle = new System.Windows.Forms.Label();
+            this.lblScaleX = new System.Windows.Forms.Label();
+            this.lblScaleY = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(500, 400);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(755, 34);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(100, 23);
+            this.btnTranslate.TabIndex = 1;
+            this.btnTranslate.Text = "Смещение";
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // btnRotate
+            // 
+            this.btnRotate.Location = new System.Drawing.Point(755, 98);
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.Size = new System.Drawing.Size(100, 23);
+            this.btnRotate.TabIndex = 2;
+            this.btnRotate.Text = "Поворот";
+            this.btnRotate.Click += new System.EventHandler(this.btnRotate_Click);
+            // 
+            // btnScale
+            // 
+            this.btnScale.Location = new System.Drawing.Point(755, 158);
+            this.btnScale.Name = "btnScale";
+            this.btnScale.Size = new System.Drawing.Size(100, 23);
+            this.btnScale.TabIndex = 3;
+            this.btnScale.Text = "Масштаб";
+            this.btnScale.Click += new System.EventHandler(this.btnScale_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(620, 250);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtDx
+            // 
+            this.txtDx.Location = new System.Drawing.Point(620, 20);
+            this.txtDx.Name = "txtDx";
+            this.txtDx.Size = new System.Drawing.Size(100, 20);
+            this.txtDx.TabIndex = 5;
+            // 
+            // txtDy
+            // 
+            this.txtDy.Location = new System.Drawing.Point(620, 60);
+            this.txtDy.Name = "txtDy";
+            this.txtDy.Size = new System.Drawing.Size(100, 20);
+            this.txtDy.TabIndex = 6;
+            // 
+            // txtAngle
+            // 
+            this.txtAngle.Location = new System.Drawing.Point(620, 100);
+            this.txtAngle.Name = "txtAngle";
+            this.txtAngle.Size = new System.Drawing.Size(100, 20);
+            this.txtAngle.TabIndex = 7;
+            // 
+            // txtScaleX
+            // 
+            this.txtScaleX.Location = new System.Drawing.Point(620, 140);
+            this.txtScaleX.Name = "txtScaleX";
+            this.txtScaleX.Size = new System.Drawing.Size(100, 20);
+            this.txtScaleX.TabIndex = 8;
+            // 
+            // txtScaleY
+            // 
+            this.txtScaleY.Location = new System.Drawing.Point(620, 180);
+            this.txtScaleY.Name = "txtScaleY";
+            this.txtScaleY.Size = new System.Drawing.Size(100, 20);
+            this.txtScaleY.TabIndex = 9;
+            // 
+            // lblDx
+            // 
+            this.lblDx.Location = new System.Drawing.Point(599, 20);
+            this.lblDx.Name = "lblDx";
+            this.lblDx.Size = new System.Drawing.Size(100, 23);
+            this.lblDx.TabIndex = 10;
+            this.lblDx.Text = "dx:";
+            // 
+            // lblDy
+            // 
+            this.lblDy.Location = new System.Drawing.Point(599, 60);
+            this.lblDy.Name = "lblDy";
+            this.lblDy.Size = new System.Drawing.Size(100, 23);
+            this.lblDy.TabIndex = 11;
+            this.lblDy.Text = "dy:";
+            // 
+            // lblAngle
+            // 
+            this.lblAngle.Location = new System.Drawing.Point(587, 100);
+            this.lblAngle.Name = "lblAngle";
+            this.lblAngle.Size = new System.Drawing.Size(100, 23);
+            this.lblAngle.TabIndex = 12;
+            this.lblAngle.Text = "Угол:";
+            // 
+            // lblScaleX
+            // 
+            this.lblScaleX.Location = new System.Drawing.Point(553, 140);
+            this.lblScaleX.Name = "lblScaleX";
+            this.lblScaleX.Size = new System.Drawing.Size(100, 23);
+            this.lblScaleX.TabIndex = 13;
+            this.lblScaleX.Text = "Масштаб X:";
+            // 
+            // lblScaleY
+            // 
+            this.lblScaleY.Location = new System.Drawing.Point(553, 180);
+            this.lblScaleY.Name = "lblScaleY";
+            this.lblScaleY.Size = new System.Drawing.Size(100, 23);
+            this.lblScaleY.TabIndex = 14;
+            this.lblScaleY.Text = "Масштаб Y:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.btnRotate);
+            this.Controls.Add(this.btnScale);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtDx);
+            this.Controls.Add(this.txtDy);
+            this.Controls.Add(this.txtAngle);
+            this.Controls.Add(this.txtScaleX);
+            this.Controls.Add(this.txtScaleY);
+            this.Controls.Add(this.lblDx);
+            this.Controls.Add(this.lblDy);
+            this.Controls.Add(this.lblAngle);
+            this.Controls.Add(this.lblScaleX);
+            this.Controls.Add(this.lblScaleY);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
+        }
         #endregion
     }
 }
+
 
--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -12,9 +12,221 @@ namespace lab4
 {
     public partial class Form1 : Form
     {
+        private Polygon currentPolygon; // Храним текущий полигон
+        private bool isDrawing; // Флаг для режима рисования полигона
+
         public Form1()
         {
             InitializeComponent();
+            currentPolygon = new Polygon();
+            isDrawing = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox.BackColor = Color.White;
+            pictureBox.Paint += pictureBox_Paint;
+            pictureBox.MouseClick += pictureBox_MouseClick;
+        }
+        public static bool IsPointInPolygon(Point2D point, Polygon polygon)
+        {
+            bool inside = false;
+            int count = polygon.Vertices.Count;
+
+            for (int i = 0, j = count - 1; i < count; j = i++)
+            {
+                Point2D vi = polygon.Vertices[i];
+                Point2D vj = polygon.Vertices[j];
+
+                if ((vi.Y > point.Y) != (vj.Y > point.Y) &&
+                    (point.X < (vj.X - vi.X) * (point.Y - vi.Y) / (vj.Y - vi.Y) + vi.X))
+                {
+                    inside = !inside;
+                }
+            }
+            return inside;
+        }
+        public static string ClassifyPoint(Point2D p, Point2D a, Point2D b)
+        {
+            double result = (b.X - a.X) * (p.Y - a.Y) - (b.Y - a.Y) * (p.X - a.X);
+
+            if (result > 0)
+                return "Слева";
+            else if (result < 0)
+                return "Справа";
+            else
+                return "На линии";
+        }
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            double dx = double.Parse(txtDx.Text);
+            double dy = double.Parse(txtDy.Text);
+            currentPolygon.ApplyTransformation(AffineTransform.Translation(dx, dy));
+            pictureBox.Invalidate(); // Перерисовка
+        }
+
+        private void btnRotate_Click(object sender, EventArgs e)
+        {
+            double angle = double.Parse(txtAngle.Text);
+            Point2D center = currentPolygon.GetCentroid(); // Центр полигона
+            currentPolygon.ApplyTransformation(AffineTransform.Rotation(angle, center));
+            pictureBox.Invalidate(); // Обновление PictureBox
+        }
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                Point2D point = new Point2D(e.X, e.Y);
+                currentPolygon.AddVertex(point);
+                pictureBox.Invalidate(); // Обновляем PictureBox для перерисовки
+            }
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentPolygon.Vertices.Count > 1)
+            {
+                for (int i = 0; i < currentPolygon.Vertices.Count - 1; i++)
+                {
+                    Point2D v1 = currentPolygon.Vertices[i];
+                    Point2D v2 = currentPolygon.Vertices[i + 1];
+                    e.Graphics.DrawLine(Pens.Black, (float)v1.X, (float)v1.Y, (float)v2.X, (float)v2.Y);
+                }
+                // Замыкаем полигон
+                Point2D last = currentPolygon.Vertices.Last();
+                Point2D first = currentPolygon.Vertices.First();
+                e.Graphics.DrawLine(Pens.Black, (float)last.X, (float)last.Y, (float)first.X, (float)first.Y);
+            }
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            currentPolygon = new Polygon();
+            pictureBox.Invalidate(); // Обновление PictureBox
+        }
+
+        private void btnScale_Click(object sender, EventArgs e)
+        {
+            double scaleX = double.Parse(txtScaleX.Text);
+            double scaleY = double.Parse(txtScaleY.Text);
+            Point2D center = currentPolygon.GetCentroid(); // Центр полигона
+            currentPolygon.ApplyTransformation(AffineTransform.Scaling(scaleX, scaleY, center));
+            pictureBox.Invalidate(); // Обновление PictureBox
+        }
+
+
+
+    }
+    public class Point2D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point2D(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point2D Transform(double[,] matrix)
+        {
+            double newX = X * matrix[0, 0] + Y * matrix[0, 1] + matrix[0, 2];
+            double newY = X * matrix[1, 0] + Y * matrix[1, 1] + matrix[1, 2];
+            return new Point2D(newX, newY);
         }
     }
+    public class Polygon
+    {
+        public List<Point2D> Vertices { get; set; }
+
+        public Polygon()
+        {
+            Vertices = new List<Point2D>();
+        }
+
+        // Добавление вершины
+        public void AddVertex(Point2D point)
+        {
+            Vertices.Add(point);
+        }
+
+        // Применение трансформации ко всем вершинам полигона
+        public void ApplyTransformation(double[,] matrix)
+        {
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                Vertices[i] = Vertices[i].Transform(matrix);
+            }
+        }
+
+        // Найти центр полигона (среднее арифметическое всех вершин)
+        public Point2D GetCentroid()
+        {
+            double sumX = 0, sumY = 0;
+            foreach (var v in Vertices)
+            {
+                sumX += v.X;
+                sumY += v.Y;
+            }
+            return new Point2D(sumX / Vertices.Count, sumY / Vertices.Count);
+        }
+    }
+    public static class AffineTransform
+    {
+        public static double[,] Translation(double dx, double dy)
+        {
+            return new double[,]
+            {
+            { 1, 0, dx },
+            { 0, 1, dy },
+            { 0, 0, 1 }
+            };
+        }
+
+        public static double[,] Rotation(double angle, Point2D center)
+        {
+            double radians = angle * Math.PI / 180.0;
+            double cos = Math.Cos(radians);
+            double sin = Math.Sin(radians);
+
+            return new double[,]
+            {
+            { cos, -sin, center.X - center.X * cos + center.Y * sin },
+            { sin, cos, center.Y - center.X * sin - center.Y * cos },
+            { 0, 0, 1 }
+            };
+        }
+
+        public static double[,] Scaling(double sx, double sy, Point2D center)
+        {
+            return new double[,]
+            {
+            { sx, 0, center.X - sx * center.X },
+            { 0, sy, center.Y - sy * center.Y },
+            { 0, 0, 1 }
+            };
+        }
+    }
+    public class Geometry
+    {
+        public static Point2D FindIntersection(Point2D p1, Point2D p2, Point2D p3, Point2D p4)
+        {
+            double d = (p1.X - p2.X) * (p3.Y - p4.Y) - (p1.Y - p2.Y) * (p3.X - p4.X);
+
+            if (d == 0) return null; // Линии параллельны
+
+            double xi = ((p3.X - p4.X) * (p1.X * p2.Y - p1.Y * p2.X) - (p1.X - p2.X) * (p3.X * p4.Y - p3.Y * p4.X)) / d;
+            double yi = ((p3.Y - p4.Y) * (p1.X * p2.Y - p1.Y * p2.X) - (p1.Y - p2.Y) * (p3.X * p4.Y - p3.Y * p4.X)) / d;
+
+            if (xi < Math.Min(p1.X, p2.X) || xi > Math.Max(p1.X, p2.X) ||
+                xi < Math.Min(p3.X, p4.X) || xi > Math.Max(p3.X, p4.X))
+                return null;
+
+            if (yi < Math.Min(p1.Y, p2.Y) || yi > Math.Max(p1.Y, p2.Y) ||
+                yi < Math.Min(p3.Y, p4.Y) || yi > Math.Max(p3.Y, p4.Y))
+                return null;
+
+            return new Point2D(xi, yi);
+        }
+    }
+    
 }
