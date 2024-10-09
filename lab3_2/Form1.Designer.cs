8d81f7060f6cd046d1280fcda274c0b2a7bf1412
PD_Creator <anuchin@sfedu.ru>
2024-10-09 10:56:11 +0300

    changed solution to c#
    Co-Authored-By: Anastasia Roy <89418251+sicknostya@users.noreply.github.com>

---
+++ lab3_2/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(163, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 579);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(13, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 19);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Брезенхем";
+            this.radioButton1.UseMnemonic = false;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(13, 38);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(38, 19);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ву";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(35, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormTask2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 578);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormTask2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Задание 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ lab3_2/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bm;
+        Graphics g;
+        List<Point> points;
+        const int MAX_COLOR_VALUE = 255;
+        Color penColor = Color.Black;
+        Pen pen;
+        public Form1()
+        {
+            InitializeComponent();
+            bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bm;
+            Clear();
+
+            points = new List<Point>();
+            pen = new Pen(penColor);
+            g = Graphics.FromImage(bm);
+        }
+        private void Clear()
+        {
+            var g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            Clear();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                points.Clear();
+                Clear();
+            }
+            else if (radioButton2.Checked)
+            {
+                points.Clear();
+                Clear();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Point point = pictureBox1.PointToClient(Cursor.Position);
+            using (Graphics g = pictureBox1.CreateGraphics())
+                if (points.Count < 2 && !points.Contains(point))
+                {
+                    g.FillRectangle(Brushes.Black, point.X, point.Y, 2, 2);
+                    points.Add(point);
+                }
+            if (radioButton1.Checked && (points.Count == 2))
+            {
+                int x0 = points[points.Count - 2].X;
+                int y0 = points[points.Count - 2].Y;
+                int x1 = points[points.Count - 1].X;
+                int y1 = points[points.Count - 1].Y;
+                Bresenham(x0, y0, x1, y1);
+                pictureBox1.Image = bm;
+            }
+            if (radioButton2.Checked && (points.Count == 2))
+            {
+                int x0 = points[points.Count - 2].X;
+                int y0 = points[points.Count - 2].Y;
+                int x1 = points[points.Count - 1].X;
+                int y1 = points[points.Count - 1].Y;
+                Wu(x0, y0, x1, y1);
+                pictureBox1.Image = bm;
+            }
+        }
+        private void Bresenham(int x0, int y0, int x1, int y1)
+        {
+            var dx = x1 - x0;
+            var dy = y1 - y0;
+            var distX = Math.Abs(dx);
+            var distY = Math.Abs(dy);
+            var stepX = dx < 0 ? -1 : 1;
+            var stepY = dy < 0 ? -1 : 1;
+            var dist = distX;
+            if (distY >= distX)
+            {
+                dist = distY;
+            }
+            var errX = 0;
+            var errY = 0;
+            var dst = dist + 1;
+            while (dst-- > 0)
+            {
+                bm.SetPixel(x0, y0, Color.Black);
+
+                errX += distX;
+                errY += distY;
+
+                if (errX >= dist)
+                {
+                    errX -= dist;
+                    x0 += stepX;
+                }
+                if (errY >= dist)
+                {
+                    errY -= dist;
+                    y0 += stepY;
+                }
+            }
+        }
+        private void Wu(int x0, int y0, int x1, int y1)
+        {
+            if (x0 > x1)
+            {
+                Swap(ref x0, ref x1);
+                Swap(ref y0, ref y1);
+            }
+
+            int dx = x1 - x0;
+            int dy = y1 - y0;
+            double gradient;
+            if (dx == 0)
+            {
+                gradient = 1;
+            }
+            else if (dy == 0)
+            {
+                gradient = 0;
+            }
+            else
+            {
+                gradient = dy / (double)dx;
+            }
+
+            int step = 1;
+            double xi = x0;
+            double yi = y0;
+
+            if (Math.Abs(gradient) > 1)
+            {
+                gradient = 1 / gradient;
+                if (gradient < 0)
+                {
+                    xi = x1;
+                    step = -1;
+                    Swap(ref y0, ref y1);
+                }
+
+                for (yi = y0; yi <= y1; yi += 1)
+                {
+                    int help;
+                    if (gradient < 0)
+                    {
+                        help = (int)(MAX_COLOR_VALUE * (xi - (int)xi));
+                    }
+                    else
+                    {
+                        help = MAX_COLOR_VALUE - (int)(MAX_COLOR_VALUE * (xi - (int)xi));
+                    }
+                    bm.SetPixel((int)xi, (int)yi, Color.FromArgb(MAX_COLOR_VALUE - help, MAX_COLOR_VALUE - help, MAX_COLOR_VALUE - help));
+                    bm.SetPixel((int)xi + step, (int)yi, Color.FromArgb(help, help, help));
+                    xi += gradient;
+                }
+            }
+            else
+            {
+                if (gradient < 0)
+                {
+                    step = -1;
+                }
+                for (xi = x0; xi <= x1; xi += 1)
+                {
+                    int help;
+                    if (gradient < 0)
+                    {
+                        help = (int)(MAX_COLOR_VALUE * (yi - (int)yi));
+                    }
+                    else
+                    {
+                        help = MAX_COLOR_VALUE - (int)(MAX_COLOR_VALUE * (yi - (int)yi));
+                    }
+                    bm.SetPixel((int)xi, (int)yi, Color.FromArgb(MAX_COLOR_VALUE - help, MAX_COLOR_VALUE - help, MAX_COLOR_VALUE - help));
+                    bm.SetPixel((int)xi, (int)yi + step, Color.FromArgb(help, help, help));
+                    yi += gradient;
+                }
+            }
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
