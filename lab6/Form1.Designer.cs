af754c6048fdc2b978f12e0ba49510cd0ef72f7a
PD_Creator <anuchin@sfedu.ru>
2024-11-05 19:56:14 +0300

    added most of functional
    Co-Authored-By: Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
    Co-Authored-By: Anastasia Roy <89418251+sicknostya@users.noreply.github.com>

---
+++ lab6/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace lab6
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        // Компоненты интерфейса
+        private System.Windows.Forms.Button ScaleButton;
+        private System.Windows.Forms.Button OffsetButton;
+        private System.Windows.Forms.Button RotateXButton;
+        private System.Windows.Forms.Button RotateYButton;
+        private System.Windows.Forms.Button RotateZButton;
+        private System.Windows.Forms.Button ReflectXYButton;
+        private System.Windows.Forms.Button ReflectXZButton;
+        private System.Windows.Forms.Button ReflectYZButton;
+        private System.Windows.Forms.ComboBox ProjectionComboBox;
+        private System.Windows.Forms.TextBox ScaleTextBox;
+        private System.Windows.Forms.TextBox RotateTextBox;
+        private System.Windows.Forms.TextBox OffsetXTextBox;
+        private System.Windows.Forms.TextBox OffsetYTextBox;
+        private System.Windows.Forms.TextBox OffsetZTextBox;
+        private System.Windows.Forms.Button TetrahedronButton;
+        private System.Windows.Forms.Button HexahedronButton;
+        private System.Windows.Forms.Button OctahedronButton;
+
+        /// <summary>
+        /// Освободить используемые ресурсы
+        /// </summary>
+        /// <param name="disposing">true если управляемый ресурс должен быть удален; иначе false</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Метод для инициализации компонентов интерфейса
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ScaleButton = new System.Windows.Forms.Button();
+            this.OffsetButton = new System.Windows.Forms.Button();
+            this.RotateXButton = new System.Windows.Forms.Button();
+            this.RotateYButton = new System.Windows.Forms.Button();
+            this.RotateZButton = new System.Windows.Forms.Button();
+            this.ReflectXYButton = new System.Windows.Forms.Button();
+            this.ReflectXZButton = new System.Windows.Forms.Button();
+            this.ReflectYZButton = new System.Windows.Forms.Button();
+            this.ProjectionComboBox = new System.Windows.Forms.ComboBox();
+            this.ScaleTextBox = new System.Windows.Forms.TextBox();
+            this.RotateTextBox = new System.Windows.Forms.TextBox();
+            this.OffsetXTextBox = new System.Windows.Forms.TextBox();
+            this.OffsetYTextBox = new System.Windows.Forms.TextBox();
+            this.OffsetZTextBox = new System.Windows.Forms.TextBox();
+            this.TetrahedronButton = new System.Windows.Forms.Button();
+            this.HexahedronButton = new System.Windows.Forms.Button();
+            this.OctahedronButton = new System.Windows.Forms.Button();
+
+            // 
+            // ScaleButton
+            // 
+            this.ScaleButton.Location = new System.Drawing.Point(10, 10);
+            this.ScaleButton.Size = new System.Drawing.Size(75, 23);
+            this.ScaleButton.Text = "Масштаб";
+            this.ScaleButton.Click += new System.EventHandler(this.ScaleButton_Click);
+
+            // 
+            // OffsetButton
+            // 
+            this.OffsetButton.Location = new System.Drawing.Point(10, 40);
+            this.OffsetButton.Size = new System.Drawing.Size(75, 23);
+            this.OffsetButton.Text = "Смещение";
+            this.OffsetButton.Click += new System.EventHandler(this.OffsetButton_Click);
+
+            // 
+            // RotateXButton
+            // 
+            this.RotateXButton.Location = new System.Drawing.Point(10, 70);
+            this.RotateXButton.Size = new System.Drawing.Size(75, 23);
+            this.RotateXButton.Text = "Поворот X";
+            this.RotateXButton.Click += new System.EventHandler(this.RotateXButton_Click);
+
+            // 
+            // RotateYButton
+            // 
+            this.RotateYButton.Location = new System.Drawing.Point(10, 100);
+            this.RotateYButton.Size = new System.Drawing.Size(75, 23);
+            this.RotateYButton.Text = "Поворот Y";
+            this.RotateYButton.Click += new System.EventHandler(this.RotateYButton_Click);
+
+            // 
+            // RotateZButton
+            // 
+            this.RotateZButton.Location = new System.Drawing.Point(10, 130);
+            this.RotateZButton.Size = new System.Drawing.Size(75, 23);
+            this.RotateZButton.Text = "Поворот Z";
+            this.RotateZButton.Click += new System.EventHandler(this.RotateZButton_Click);
+
+            // 
+            // ReflectXYButton
+            // 
+            this.ReflectXYButton.Location = new System.Drawing.Point(10, 160);
+            this.ReflectXYButton.Size = new System.Drawing.Size(75, 23);
+            this.ReflectXYButton.Text = "Отразить XY";
+            this.ReflectXYButton.Click += new System.EventHandler(this.ReflectXYButton_Click);
+
+            // 
+            // ReflectXZButton
+            // 
+            this.ReflectXZButton.Location = new System.Drawing.Point(10, 190);
+            this.ReflectXZButton.Size = new System.Drawing.Size(75, 23);
+            this.ReflectXZButton.Text = "Отразить XZ";
+            this.ReflectXZButton.Click += new System.EventHandler(this.ReflectXZButton_Click);
+
+            // 
+            // ReflectYZButton
+            // 
+            this.ReflectYZButton.Location = new System.Drawing.Point(10, 220);
+            this.ReflectYZButton.Size = new System.Drawing.Size(75, 23);
+            this.ReflectYZButton.Text = "Отразить YZ";
+            this.ReflectYZButton.Click += new System.EventHandler(this.ReflectYZButton_Click);
+
+            // 
+            // ProjectionComboBox
+            // 
+            this.ProjectionComboBox.Location = new System.Drawing.Point(10, 250);
+            this.ProjectionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ProjectionComboBox.Items.AddRange(new object[] { "Perspective", "Axonometric" });
+            this.ProjectionComboBox.SelectedIndexChanged += new System.EventHandler(this.ProjectionComboBox_SelectedIndexChanged);
+
+            // 
+            // ScaleTextBox
+            // 
+            this.ScaleTextBox.Location = new System.Drawing.Point(100, 10);
+            this.ScaleTextBox.Size = new System.Drawing.Size(50, 20);
+
+            // 
+            // RotateTextBox
+            // 
+            this.RotateTextBox.Location = new System.Drawing.Point(100, 40);
+            this.RotateTextBox.Size = new System.Drawing.Size(50, 20);
+
+            // 
+            // OffsetXTextBox
+            // 
+            this.OffsetXTextBox.Location = new System.Drawing.Point(100, 70);
+            this.OffsetXTextBox.Size = new System.Drawing.Size(50, 20);
+
+            // 
+            // OffsetYTextBox
+            // 
+            this.OffsetYTextBox.Location = new System.Drawing.Point(100, 100);
+            this.OffsetYTextBox.Size = new System.Drawing.Size(50, 20);
+
+            // 
+            // OffsetZTextBox
+            // 
+            this.OffsetZTextBox.Location = new System.Drawing.Point(100, 130);
+            this.OffsetZTextBox.Size = new System.Drawing.Size(50, 20);
+
+            // 
+            // TetrahedronButton
+            // 
+            this.TetrahedronButton.Location = new System.Drawing.Point(10, 280);
+            this.TetrahedronButton.Size = new System.Drawing.Size(75, 23);
+            this.TetrahedronButton.Text = "Тетраэдр";
+            this.TetrahedronButton.Click += new System.EventHandler(this.TetrahedronButton_Click);
+
+            // 
+            // HexahedronButton
+            // 
+            this.HexahedronButton.Location = new System.Drawing.Point(10, 310);
+            this.HexahedronButton.Size = new System.Drawing.Size(75, 23);
+            this.HexahedronButton.Text = "Гексаэдр";
+            this.HexahedronButton.Click += new System.EventHandler(this.HexahedronButton_Click);
+
+            // 
+            // OctahedronButton
+            // 
+            this.OctahedronButton.Location = new System.Drawing.Point(10, 340);
+            this.OctahedronButton.Size = new System.Drawing.Size(75, 23);
+            this.OctahedronButton.Text = "Октаэдр";
+            this.OctahedronButton.Click += new System.EventHandler(this.OctahedronButton_Click);
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.ScaleButton);
+            this.Controls.Add(this.OffsetButton);
+            this.Controls.Add(this.RotateXButton);
+            this.Controls.Add(this.RotateYButton);
+            this.Controls.Add(this.RotateZButton);
+            this.Controls.Add(this.ReflectXYButton);
+            this.Controls.Add(this.ReflectXZButton);
+            this.Controls.Add(this.ReflectYZButton);
+            this.Controls.Add(this.ProjectionComboBox);
+            this.Controls.Add(this.ScaleTextBox);
+            this.Controls.Add(this.RotateTextBox);
+            this.Controls.Add(this.OffsetXTextBox);
+            this.Controls.Add(this.OffsetYTextBox);
+            this.Controls.Add(this.OffsetZTextBox);
+            this.Controls.Add(this.TetrahedronButton);
+            this.Controls.Add(this.HexahedronButton);
+            this.Controls.Add(this.OctahedronButton);
+            this.Text = "Polyhedron Transformations";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+        }
+    }
+}
---
+++ lab6/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab6
+{
+    public partial class Form1 : Form
+    {
+        private Polyhedron polyhedron;
+        private ProjectionType projection = ProjectionType.Perspective;
+
+        public Form1()
+        {
+            InitializeComponent();
+            polyhedron = new Polyhedron();
+            polyhedron.CreateTetrahedron(); // По умолчанию отображаем тетраэдр
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            polyhedron.Draw(e.Graphics, ClientRectangle, projection);
+        }
+
+        private void ScaleButton_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(ScaleTextBox.Text, out float factor))
+            {
+                polyhedron.Scale(factor);
+                Invalidate();
+            }
+        }
+
+        private void OffsetButton_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(OffsetXTextBox.Text, out float dx) &&
+                float.TryParse(OffsetYTextBox.Text, out float dy) &&
+                float.TryParse(OffsetZTextBox.Text, out float dz))
+            {
+                polyhedron.Offset(dx, dy, dz);
+                Invalidate();
+            }
+        }
+
+        private void RotateXButton_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(RotateTextBox.Text, out float angle))
+            {
+                polyhedron.RotateAroundAxis(Axis.X, angle);
+                Invalidate();
+            }
+        }
+
+        private void RotateYButton_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(RotateTextBox.Text, out float angle))
+            {
+                polyhedron.RotateAroundAxis(Axis.Y, angle);
+                Invalidate();
+            }
+        }
+
+        private void RotateZButton_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(RotateTextBox.Text, out float angle))
+            {
+                polyhedron.RotateAroundAxis(Axis.Z, angle);
+                Invalidate();
+            }
+        }
+
+        private void ReflectXYButton_Click(object sender, EventArgs e)
+        {
+            polyhedron.Reflect(Axis.Z);
+            Invalidate();
+        }
+
+        private void ReflectXZButton_Click(object sender, EventArgs e)
+        {
+            polyhedron.Reflect(Axis.Y);
+            Invalidate();
+        }
+
+        private void ReflectYZButton_Click(object sender, EventArgs e)
+        {
+            polyhedron.Reflect(Axis.X);
+            Invalidate();
+        }
+
+        private void ProjectionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ProjectionComboBox.SelectedItem.ToString() == "Perspective")
+                projection = ProjectionType.Perspective;
+            else
+                projection = ProjectionType.Axonometric;
+
+            Invalidate();
+        }
+
+        private void TetrahedronButton_Click(object sender, EventArgs e)
+        {
+            polyhedron.CreateTetrahedron();
+            Invalidate();
+        }
+
+        private void HexahedronButton_Click(object sender, EventArgs e)
+        {
+            polyhedron.CreateHexahedron();
+            Invalidate();
+        }
+
+        private void OctahedronButton_Click(object sender, EventArgs e)
+        {
+            polyhedron.CreateOctahedron();
+            Invalidate();
+        }
+    }
+
+    public class Point3D
+    {
+        public float X, Y, Z;
+
+        public Point3D(float x, float y, float z)
+        {
+            X = x; Y = y; Z = z;
+        }
+
+        public Point3D Transform(float[,] matrix)
+        {
+            float newX = X * matrix[0, 0] + Y * matrix[0, 1] + Z * matrix[0, 2] + matrix[0, 3];
+            float newY = X * matrix[1, 0] + Y * matrix[1, 1] + Z * matrix[1, 2] + matrix[1, 3];
+            float newZ = X * matrix[2, 0] + Y * matrix[2, 1] + Z * matrix[2, 2] + matrix[2, 3];
+            return new Point3D(newX, newY, newZ);
+        }
+    }
+
+    public class Polygon
+    {
+        public List<Point3D> Points;
+
+        public Polygon(List<Point3D> points)
+        {
+            Points = points;
+        }
+    }
+
+    public class Polyhedron
+    {
+        private List<Polygon> faces = new List<Polygon>();
+
+        public void CreateTetrahedron()
+        {
+            Point3D p1 = new Point3D(1, 1, 1);
+            Point3D p2 = new Point3D(-1, -1, 1);
+            Point3D p3 = new Point3D(-1, 1, -1);
+            Point3D p4 = new Point3D(1, -1, -1);
+
+            faces.Clear();
+            faces.Add(new Polygon(new List<Point3D> { p1, p2, p3 }));
+            faces.Add(new Polygon(new List<Point3D> { p1, p2, p4 }));
+            faces.Add(new Polygon(new List<Point3D> { p1, p3, p4 }));
+            faces.Add(new Polygon(new List<Point3D> { p2, p3, p4 }));
+        }
+
+        public void CreateHexahedron()
+        {
+            Point3D p1 = new Point3D(-1, -1, -1);
+            Point3D p2 = new Point3D(1, -1, -1);
+            Point3D p3 = new Point3D(1, 1, -1);
+            Point3D p4 = new Point3D(-1, 1, -1);
+            Point3D p5 = new Point3D(-1, -1, 1);
+            Point3D p6 = new Point3D(1, -1, 1);
+            Point3D p7 = new Point3D(1, 1, 1);
+            Point3D p8 = new Point3D(-1, 1, 1);
+
+            faces.Clear();
+            faces.Add(new Polygon(new List<Point3D> { p1, p2, p3, p4 }));
+            faces.Add(new Polygon(new List<Point3D> { p5, p6, p7, p8 }));
+            faces.Add(new Polygon(new List<Point3D> { p1, p2, p6, p5 }));
+            faces.Add(new Polygon(new List<Point3D> { p2, p3, p7, p6 }));
+            faces.Add(new Polygon(new List<Point3D> { p3, p4, p8, p7 }));
+            faces.Add(new Polygon(new List<Point3D> { p4, p1, p5, p8 }));
+        }
+
+        public void CreateOctahedron()
+        {
+            Point3D p1 = new Point3D(0, 0, 1);
+            Point3D p2 = new Point3D(1, 0, 0);
+            Point3D p3 = new Point3D(0, 1, 0);
+            Point3D p4 = new Point3D(-1, 0, 0);
+            Point3D p5 = new Point3D(0, -1, 0);
+            Point3D p6 = new Point3D(0, 0, -1);
+
+            faces.Clear();
+            faces.Add(new Polygon(new List<Point3D> { p1, p2, p3 }));
+            faces.Add(new Polygon(new List<Point3D> { p1, p3, p4 }));
+            faces.Add(new Polygon(new List<Point3D> { p1, p4, p5 }));
+            faces.Add(new Polygon(new List<Point3D> { p1, p5, p2 }));
+            faces.Add(new Polygon(new List<Point3D> { p6, p3, p2 }));
+            faces.Add(new Polygon(new List<Point3D> { p6, p4, p3 }));
+            faces.Add(new Polygon(new List<Point3D> { p6, p5, p4 }));
+            faces.Add(new Polygon(new List<Point3D> { p6, p2, p5 }));
+        }
+
+        public void Draw(Graphics g, Rectangle clientRect, ProjectionType projection)
+        {
+            foreach (var face in faces)
+            {
+                Point[] points = new Point[face.Points.Count];
+                for (int i = 0; i < face.Points.Count; i++)
+                {
+                    Point3D point = face.Points[i];
+
+                    if (projection == ProjectionType.Perspective)
+                    {
+                        float scale = 300 / (point.Z + 5);
+                        int x = (int)(clientRect.Width / 2 + point.X * scale);
+                        int y = (int)(clientRect.Height / 2 - point.Y * scale);
+                        points[i] = new Point(x, y);
+                    }
+                    else if (projection == ProjectionType.Axonometric)
+                    {
+                        int x = (int)(clientRect.Width / 2 + point.X * 40 - point.Z * 20);
+                        int y = (int)(clientRect.Height / 2 - point.Y * 40 - point.Z * 10);
+                        points[i] = new Point(x, y);
+                    }
+                }
+                g.DrawPolygon(Pens.Black, points);
+            }
+        }
+
+        public void Scale(float factor)
+        {
+            // Матрица масштабирования
+            float[,] scaleMatrix = {
+                { factor, 0, 0, 0 },
+                { 0, factor, 0, 0 },
+                { 0, 0, factor, 0 },
+                { 0, 0, 0, 1 }
+            };
+            Transform(scaleMatrix);
+        }
+
+        public void Offset(float dx, float dy, float dz)
+        {
+            float[,] offsetMatrix = {
+                { 1, 0, 0, dx },
+                { 0, 1, 0, dy },
+                { 0, 0, 1, dz },
+                { 0, 0, 0, 1 }
+            };
+            Transform(offsetMatrix);
+        }
+
+        public void RotateAroundAxis(Axis axis, float angle)
+        {
+            float radians = angle * (float)Math.PI / 180;
+            float cos = (float)Math.Cos(radians);
+            float sin = (float)Math.Sin(radians);
+
+            float[,] rotationMatrix;
+            if (axis == Axis.X)
+            {
+                rotationMatrix = new float[,] {
+                    { 1, 0, 0, 0 },
+                    { 0, cos, -sin, 0 },
+                    { 0, sin, cos, 0 },
+                    { 0, 0, 0, 1 }
+                };
+            }
+            else if (axis == Axis.Y)
+            {
+                rotationMatrix = new float[,] {
+                    { cos, 0, sin, 0 },
+                    { 0, 1, 0, 0 },
+                    { -sin, 0, cos, 0 },
+                    { 0, 0, 0, 1 }
+                };
+            }
+            else
+            {
+                rotationMatrix = new float[,] {
+                    { cos, -sin, 0, 0 },
+                    { sin, cos, 0, 0 },
+                    { 0, 0, 1, 0 },
+                    { 0, 0, 0, 1 }
+                };
+            }
+            Transform(rotationMatrix);
+        }
+
+        public void Reflect(Axis axis)
+        {
+            float[,] reflectMatrix;
+            if (axis == Axis.X)
+            {
+                reflectMatrix = new float[,] {
+                    { -1, 0, 0, 0 },
+                    { 0, 1, 0, 0 },
+                    { 0, 0, 1, 0 },
+                    { 0, 0, 0, 1 }
+                };
+            }
+            else if (axis == Axis.Y)
+            {
+                reflectMatrix = new float[,] {
+                    { 1, 0, 0, 0 },
+                    { 0, -1, 0, 0 },
+                    { 0, 0, 1, 0 },
+                    { 0, 0, 0, 1 }
+                };
+            }
+            else
+            {
+                reflectMatrix = new float[,] {
+                    { 1, 0, 0, 0 },
+                    { 0, 1, 0, 0 },
+                    { 0, 0, -1, 0 },
+                    { 0, 0, 0, 1 }
+                };
+            }
+            Transform(reflectMatrix);
+        }
+
+        private void Transform(float[,] matrix)
+        {
+            for (int i = 0; i < faces.Count; i++)
+            {
+                for (int j = 0; j < faces[i].Points.Count; j++)
+                {
+                    faces[i].Points[j] = faces[i].Points[j].Transform(matrix);
+                }
+            }
+        }
+    }
+
+    public enum Axis { X, Y, Z }
+    public enum ProjectionType { Perspective, Axonometric }
+}
